c0e3b0607c05dfdd0b56028540b7101ef0848167
ovchinikov <sergeyvinokov@gmail.com>
2024-01-10 06:44:02 +0300

    exercise 3.15, 3.16, 3.18

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const cors = require("cors");
+const Person = require("./models/personsSchema");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -18,95 +21,98 @@ app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 
-const data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// errorHandler
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "404 not found" });
+};
+
 // get all persons
-app.get("/api/persons", (req, res) => {
-  res.json(data);
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 // display info about the phonebook status
 app.get("/info", (req, res) => {
-  const count = data.length;
   const now = new Date().toUTCString();
-  res.send(`<p>Phonebook has info for ${count} people </br> ${now} </p>`);
+  Person.find({}).then((persons) => {
+    res.send(
+      `<p>Phonebook has info for ${persons.length} people</p><p>${now}</p>`
+    );
+  });
 });
 
 // get a single person
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const person = data.find((data) => data.id === id);
-  if (person) {
-    res.json(person);
-  }
-  res.status(404).json({ message: "404 not found" });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => next(error));
+});
+
+// update a person
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, newPerson, { new: true })
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 // delete resource
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const person = data.find((data) => data.id === id);
-  res.json({ person, message: "Removed person successfully" });
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .then((error) => next(error));
 });
 
 // create new contact
 
-const generateId = () => {
-  return Math.floor(Math.random() * 10000);
-};
-
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   const person = {
-    id: generateId(),
     name: body.name,
     number: body.number,
   };
 
-  const name = data.find((data) => data.name === body.name);
-  const number = data.find((data) => data.number === body.number);
+  Person.create(person)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => next(error));
 
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "content missing",
     });
   }
-
-  if (name) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  if (number) {
-    return res.status(400).json({
-      error: "number must be unique",
-    });
-  }
-
-  data.concat(person);
-  res.json(person);
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
---
+++ phonebook/models/personsSchema.js
@@ -0,0 +1,25 @@
+const { mongoose } = require("mongoose");
+require("dotenv").config();
+const url = process.env.MONGO_URI;
+console.log(url);
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(url)
+  .then((res) => console.log("Connected to Mongodb"))
+  .catch((err) => console.error(err));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
